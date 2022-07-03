02dd7ef19bd3ea420fdbce46e3abc2de0140db3e
bishal <bishal.sukubhatu01@gmail.com>
2022-07-03 14:58:40 +0545

    corrected developer page

--- src/pages/Developers.page.js
+++ src/pages/Developers.page.js
@@ -41,17 +41,17 @@ const Developers = () => {
 
     {
       img: "https://images.unsplash.com/photo-1656634064343-39ff5269d651?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
-      title: "Savyata",
+      title: "Dhirendra",
       des: "Dev",
     },
     {
       img: "https://images.unsplash.com/photo-1656582117878-165d992214f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
-      title: "Boka ko xora",
+      title: "Pushpa",
       des: "Dev",
     },
     {
       img: "https://images.unsplash.com/photo-1656634064343-39ff5269d651?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
-      title: "Boka ko xori",
+      title: "Bahubali",
       des: "Dev",
     },
   ];
